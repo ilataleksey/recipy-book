406f2df8e42002e0a7c34cf1102d2ba7e0ccdff3
Aleksey <48519689+ilataleksey@users.noreply.github.com>
2021-12-18 22:47:40 +0300

    Update settings.py

--- recipyBook/recipyBook/settings.py
+++ recipyBook/recipyBook/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*jzf%v$vspk))0d-(m)%5sz-f39=9)5n-r&57_)_)7=9bn8q=x'
+SECRET_KEY = ''
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
