bada575e23da415c0122593c6993a317c3abe279
ilataleksey <ilataleksey@gmail.com>
2021-12-19 13:26:37 +0300

    Реализует передачу данных в шаблоны

--- recipyBook/main/views.py
+++ recipyBook/main/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['some', 'text', 'or', 'massive', 'of', 'data', 'and', 'numbers', '1234567890'],
+        'obj': {
+            'car': 'BMW',
+            'age': 18,
+            'hobby': 'football',
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 
 def about(request):
