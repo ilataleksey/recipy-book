a559fbcfe93e171ac1352afb53aa1a56327ba6d8
ilataleksey <ilataleksey@gmail.com>
2021-12-19 15:40:03 +0300

    Создает и подключает модели (БД) к проекту

--- recipyBook/recipes/models.py
+++ recipyBook/recipes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipes(models.Model):
+    title = models.CharField('Название', max_length=50)
+    anons = models.CharField('Анонс', max_length=250)
+    full_text = models.TextField('Рецепт')
+    date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta():
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
