2622e4bf2a32e58c437421881ea24f49f8d6e13d
ilataleksey <ilataleksey@gmail.com>
2022-01-01 17:15:28 +0300

    Add on page render recipes generated by Recipes Class

--- recipyBook/recipes/views.py
+++ recipyBook/recipes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Recipes
 
 
 def recipes_home(request):
-    return render(request, 'recipes/recipes_home.html')
+    recipes = Recipes.objects.order_by('-date')
+    return render(request, 'recipes/recipes_home.html', {'recipes': recipes})
